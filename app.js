3de50d19b7b036df54a67604edec4056dbc98182
Robert Cordray <robertcordray@gmail.com>
2017-08-22 07:31:44 -0500

    catching up

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+var geeseArray = [{
+        name: 'Fred',
+        age: 5,
+        type: 'Betelgeusian Goosian'
+    },
+    {
+        name: 'Carla',
+        age: 4,
+        type: 'Canadian'
+    }
+];
+
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-var port = 5000
+var port = 5000;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function() {});
+app.get('/geese', function(req, res) {
+    res.send(geeseArray);
+});
+
+app.post('/geese', function(req, res) {
+    console.log(req.body);
+    if (req.body.name === '') {
+        res.send('honk');
+    }
+    geeseArray.push(req.body);
+    res.sendStatus(201);
+});
+
+app.listen(port, function() {
+    console.log('Running on port', port);
+});
---
+++ public/client.js
@@ -0,0 +1,62 @@
+console.log('client.js has been loaded');
+
+var geese = [];
+
+$(document).ready(function() {
+            console.log('jquery is loaded');
+
+            // Add new goose on button click
+            $('#newGooseButton').on('click', function() {
+                        console.log('new goose button was clicked');
+                        var newGoose = {}; // creates empty newGoose object
+
+                        // Adding properties to the new goose
+                        newGoose.name = $('#newGooseName').val();
+                        newGoose.age = parseInt($('#newGooseAge').val());
+                        newGoose.type = $('#newGooseType').val();
+
+
+
+                        console.log('newGoose', newGoose);
+
+                        $.ajax({
+                                method: 'POST',
+                                url: '/geese',
+                                data: newGoose,
+                                success: function(response) {
+                                    console.log(response);
+                                    getGeesen()
+                                        // redrawing the table with the new goose
+                                },
+                                drawGeeseTable();
+                            },
+
+
+                            function getGeesen() {
+                                $.ajax({
+                                    method: 'GET',
+                                    url: '/geese',
+                                    success: function(response) { // response will be the array of geese
+                                        console.log(response);
+                                        geese = response;
+                                        drawGeeseTable();
+                                    }
+                                });
+                            };
+
+                            function drawGeeseTable() {
+                                // adding geese to DOM
+                                $('#geesenTableBody').empty();
+                                for (var i = 0; i < geese.length; i++) {
+                                    var goose = geese[i];
+
+                                    $('#geesenTableBody').prepend(
+                                        '<tr>' +
+                                        '<td>' + goose.name + '</td>' +
+                                        '<td>' + goose.age + '</td>' +
+                                        '<td>' + goose.type + '</td>' +
+                                        '</tr>'
+                                    );
+                                }
+                            }
+                        });
